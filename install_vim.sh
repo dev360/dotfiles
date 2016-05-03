58698c5e4b6bcf8daead3cab2a7c3fc01c43dab1
Christian Toivola <c.toivola@gmail.com>
2016-05-03 11:51:26 -0400

    Added install script

---
+++ install_vim.sh
@@ -0,0 +1,17 @@
+cp -rf .vim ~/.vim
+cp -f .vimrc ~/.vimrc
+
+if [ ! -f ~/.vimrc.local ];
+then
+    cp .vimrc.local ~/.vimrc.local
+else
+    read -p "Overwrite ~/.vimrc.local? y/n " -n 1 -r
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        cp -f .vimrc.local ~/.vimrc.local
+    fi
+fi
+
+
+echo "Vim settings copied successfully"
