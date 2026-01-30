448984b3f4de14b2427ecd5943f632fd740f4682
Christian Toivola <c.toivola@gmail.com>
2026-01-30 13:42:10 -0500

    Add themes configuration for Neovim plugins

---
+++ .config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,8 @@
+return {
+  -- Add all the modern themes here
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "folke/tokyonight.nvim", priority = 1000 },
+  { "rebelot/kanagawa.nvim", priority = 1000 },
+  { "rose-pine/neovim", name = "rose-pine", priority = 1000 },
+  { "nyoom-engineering/oxocarbon.nvim", priority = 1000 },
+}
